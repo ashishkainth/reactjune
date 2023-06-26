562c1a7b50f0eaaec30ac678c0b852de7bcbf38a
ashishkainth <kainth.ashish@gmail.com>
2023-06-26 19:00:30 +0530

    minor enhancements

--- App.js
+++ App.js
@@ -3,9 +3,12 @@
 // const root = document.getElementById("root");
 // root.appendChild(heading);
 
-const div = React.createElement("div", { key: "parentDiv" }, [
-  React.createElement("p", {}, "I am paragraph1!"),
-  React.createElement("p", {}, "I am paragraph2!"),
+import React from "react";
+import ReactDOM from "react-dom";
+
+const div = React.createElement("div", { id: "div", key: "parentDiv" }, [
+  React.createElement("p", { id: "p1" }, "I am paragraph1!"),
+  React.createElement("p", { id: "p2" }, "I am paragraph2!"),
 ]);
 
 const heading1 = React.createElement(
@@ -14,7 +17,11 @@ const heading1 = React.createElement(
   "Hello World1!"
 );
 
-const heading2 = React.createElement("h1", { key: "child" }, "Hello World2!");
+const heading2 = React.createElement(
+  "h1",
+  { id: "heading2", key: "child" },
+  "Hello World2!"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
